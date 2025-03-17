9344db8931c2824bf30d8f5fa069a0277112e754
Tommy Burkett <158101222+tommy-burkett@users.noreply.github.com>
2025-03-17 16:22:54 -0400

    Update basicpage.py

--- src/basicpage.py
+++ src/basicpage.py
@@ -1,6 +1,6 @@
 # Dash: The application class
 # html: Various HTML components
-# dcc: dash core components (things like buttons, figures, slider bars, etc.)
+# dcc: Dash core components (things like buttons, figures, slider bars, etc.)
 from dash import Dash, html, dcc
 
 from src.analysis import get_data
@@ -11,12 +11,13 @@ def run_app() -> None:
     app = Dash(__name__)
 
     # The title will appear in the browser tab
-    app.title = "Hello World"
+    app.title = 'Hello World!'
 
     # Create a layout
     create_layout(app)
 
-    # This runs the app, only use debug=True when testing, it shows an additional menu on the webpage
+    # This runs the app. 
+    # Only include debug=True when testing, it shows an additional menu on the webpage
     app.run(debug=True)
     return None
 
@@ -27,9 +28,12 @@ def make_figure() -> go.Figure:
 
 def create_layout(app: Dash) -> None:
     layout = html.Div(id='main-div',
-                      children=[html.H1('welcome to my website'),   # This is a large header
+                      children=[html.H1('Welcome to my website!'),  # This is a large header
                                 html.Hr(),                          # This is a horizontal line
-                                html.H2('here is a figure')         # This is a smaller header
+                                html.H2('Here is a figure:'),       # This is a smaller header
+                                dcc.Graph(id='my-figure',
+                                          figure=make_figure()
+                                          )
                                 ]
                       )
     app.layout = layout
