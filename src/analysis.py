a3e585d0c8a55ee5941fb1c8caab5ed481246b52
Tommy Burkett <158101222+tommy-burkett@users.noreply.github.com>
2025-03-17 16:22:31 -0400

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -2,7 +2,7 @@ import numpy as np
 
 TAU = 2*np.pi
 
-def get_data(n: int = 100, freq: float=1) -> tuple[np.ndarray, np.ndarray]:
+def get_data(n: int = 100, freq: float = 1) -> tuple[np.ndarray, np.ndarray]:
     x = np.linspace(0, 10, n)
     y = np.sin(TAU*x*freq)
     return x, y
